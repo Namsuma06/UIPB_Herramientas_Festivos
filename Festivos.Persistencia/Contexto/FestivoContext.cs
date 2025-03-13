b748e9293bbe80b7d31bafad1a6199e760fef9df
Namsuma06 <maria.benitez753@pascualbravo.edu.co>
2025-03-13 18:22:32 -0500

    Festivos.Core

---
+++ Festivos.Persistencia/Contexto/FestivoContext.cs
@@ -0,0 +1,41 @@
+﻿using IUPBFestivos.Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace Festivos.Persistencia.Contexto
+{
+    public class FestivosContext : DbContext
+    {
+        public FestivosContext(DbContextOptions<FestivosContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Tipo> Tipos { get; set; }
+        public DbSet<Festivo> Festivos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Tipo>(entidad =>
+            {
+                entidad.HasKey(e => e.Id);
+                entidad.Property(e => e.Nombre)
+                       .IsRequired()
+                       .HasMaxLength(100);
+            });
+
+            builder.Entity<Festivo>(entidad =>
+            {
+                entidad.HasKey(e => e.Id);
+                entidad.Property(e => e.Nombre)
+                       .IsRequired()
+                       .HasMaxLength(100);
+
+                entidad.HasOne(e => e.Tipo)
+                       .WithMany(t => t.Festivos)
+                       .HasForeignKey(e => e.IdTipo)
+                       .OnDelete(DeleteBehavior.Cascade);
+            });
+        }
+    }
+}
+
---
+++ IUPBFestivos.Dominio/IUPBFestivos.Dominio/Entidades/Festivo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IUPBFestivos.Dominio.Entidades
+{
+    [Table("Festivo")]
+    public class Festivo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public int Dia { get; set; }
+        public int Mes { get; set; }
+        public int DiasPascua { get; set; }
+
+        [ForeignKey("Tipo")]
+        public int IdTipo { get; set; }
+
+        public Tipo Tipo { get; set; } // Relación con Tipo  
+    }
+}
+
